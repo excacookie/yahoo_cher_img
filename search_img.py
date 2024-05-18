22365968a8ec77e68de7dc3615e94509fa312e3f
cookie excalibure <146814855+excacookie@users.noreply.github.com>
2024-05-18 22:48:52 +0200

    Update search_img.py

--- search_img.py
+++ search_img.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup as bs
+#for jupyter notebook      
 from IPython.display import Image, display
 
 startp= "https://fr.images.search.yahoo.com/search/images;_ylt=?p="
@@ -12,19 +13,26 @@ pfinal=startp+rechplus+endp
 request = requests.get(pfinal)
 soup=bs(request.text,"html.parser")
 images = soup.find_all('img')
+count=0
 for img in images:
-    if img==None:
-        del(images[img])
+    if img.get('src')==None:
+        del(images[count])
+    count=count+1
 max_images=int(input("nombre d'image que l'on veut télécharger : "))
 i=1
-for j,img in enumerate(images[:max_images]):
-        img_url = img.get('src')
-        if img_url!=None:
-            print(img_url)
-            response = requests.get(img_url, stream=True)
-            save_path=f"image{i}.png"
-            i=i+1
-            with open(save_path, 'wb') as file:
-                for chunk in response.iter_content(1024):  
-                    file.write(chunk)
-      
+for j in range(max_images):
+    img_url = images[j].get('src')
+    print(img_url)
+    response = requests.get(img_url, stream=True)
+    save_path=f"image{i}.png"
+    i=i+1
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(1024):
+            file.write(chunk)
+#for jupyter notebook      
+limgpath = []
+for k in range(1,i):
+    limgpath.append(f'image{k}.png')
+    
+for imgpath in limgpath:
+    display(Image(filename=imgpath))
